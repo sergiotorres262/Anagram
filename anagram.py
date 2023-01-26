242983b344885a83ebae564b4f15a7a38f02518e
Sergio <sergio2002ojedatorres@gmail.com>
2023-01-26 09:11:32 +0100

    spaces

--- anagram.py
+++ anagram.py
@@ -13,11 +13,9 @@ def isAnagram(cadena1,cadena2, dicc1, dicc2):
     ayCadena2 = [char for char in cadena2.lower()]
     for i in range(len(cadena1)):
         dicc1[ayCadena1[i]] = dicc1.get(ayCadena1[i], 0) + 1
-    print(dicc1)
     
     for i in range(len(cadena2)):
         dicc2[ayCadena2[i]] = dicc2.get(ayCadena2[i], 0) + 1
-    print(dicc2)
 
     if dicc1 == dicc2:
         print(True)
